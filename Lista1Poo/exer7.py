1e12384e96ffbad87f29f90c29679e5c6893fa07
bia22barbi <barbianabb@gmail.com>
2026-07-31 16:40:20 -0300

    exer6

---
+++ Lista1Poo/exer7.py
@@ -0,0 +1,18 @@
+nota= []
+def adicionar_nota(): 
+nome = input("Digite o nome do aluno:")
+nota = float(input("Nota: "))
+disciplina = input("Disciplina:")
+
+notas.append(nome,nota,disciplina))
+print("Nota adicionada com sucesso!")
+
+def melhor_aluno():
+    if len(notas) == 0:
+        print("Nenhuma nota cadastrada.")
+        return
+
+    melhor = {}
+
+    for nome 
+
